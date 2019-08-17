11181fbf016c37355edcfe0fe488c5612feda4ee
曹桂凤 <demi.cao@oocl.com>
2019-08-17 16:48:25 +0800

    [Demi][feat:impelemnt test2 should_return_player1_win_when_input_2H_and_JH]

--- src/test/java/pokerHands/PokerHandsTest.java
+++ src/test/java/pokerHands/PokerHandsTest.java
@@ -9,20 +9,31 @@ import org.junit.Test;
 
 public class PokerHandsTest {
   @Test
-  public void should_return_player1_win_when_input_and_3H(){
+  public void should_return_player1_win_when_input_2H_and_3H() {
 
-    Card card1 = new Card("2H");
-    List<Card>cardList1 = Arrays.asList(card1);
-    Card card2 = new Card("3H");
-    List<Card>cardList2 = Arrays.asList(card2);
+    Card card1 = new Card("3H");
+    List<Card> cardList1 = Arrays.asList(card1);
+    Card card2 = new Card("2H");
+    List<Card> cardList2 = Arrays.asList(card2);
 
     PokerHands pokerHands = new PokerHands();
-    String result = pokerHands.judge(cardList1,cardList2);
+    String result = pokerHands.judge(cardList1, cardList2);
 
-    assertEquals("player 1 win!",result);
+    assertEquals("player 1 win!", result);
+  }
 
+  @Test
+  public void should_return_player1_win_when_input_2H_and_JH() {
 
+    Card card1 = new Card("2H");
+    List<Card> cardList1 = Arrays.asList(card1);
+    Card card2 = new Card("JH");
+    List<Card> cardList2 = Arrays.asList(card2);
 
+    PokerHands pokerHands = new PokerHands();
+    String result = pokerHands.judge(cardList1, cardList2);
+
+    assertEquals("player 2 win!", result);
   }
 
 }
