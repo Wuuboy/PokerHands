ea3f2bbaf13a3d15feec51d00591cd28a57fc65b
曹桂凤 <demi.cao@oocl.com>
2019-08-17 19:48:10 +0800

    [Demi][feat:impelemnt test6 should_return_player1_win_when_input_2H_3D_5S_9C_KD_and_3H_3D_5S_9C_JD]

--- src/test/java/pokerHands/PokerHandsTest.java
+++ src/test/java/pokerHands/PokerHandsTest.java
@@ -24,7 +24,6 @@ public class PokerHandsTest {
 
   @Test
   public void should_return_player1_win_when_input_2H_and_JH() {
-
     Card card1 = new Card("2H");
     List<Card> cardList1 = Arrays.asList(card1);
     Card card2 = new Card("JH");
@@ -81,7 +80,29 @@ public class PokerHandsTest {
     PokerHands pokerHands = new PokerHands();
     String result = pokerHands.judge(cardList1,cardList2);
 
-    assertEquals("player2 win!",result);
+    assertEquals("player 2 win!",result);
+  }
+
+  @Test
+  public void should_return_player1_win_when_input_2H_3D_5S_9C_KD_and_3H_3D_5S_9C_JD() {
+
+    Card card1 = new Card("2H");
+    Card card2 = new Card("3D");
+    Card card3 = new Card("5S");
+    Card card4 = new Card("9C");
+    Card card5 = new Card("KD");
+    List<Card>cardList1 = Arrays.asList(card1,card2,card3,card4,card5);
+    Card card6 = new Card("3H");
+    Card card7 = new Card("3D");
+    Card card8 = new Card("5S");
+    Card card9 = new Card("9C");
+    Card card10 = new Card("JD");
+    List<Card>cardList2 = Arrays.asList(card6,card7,card8,card9,card10);
+
+    PokerHands pokerHands = new PokerHands();
+    String result = pokerHands.judge(cardList1,cardList2);
+
+    assertEquals("player 1 win!",result);
   }
 
 }
