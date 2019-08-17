8660e37af7e4b07621399333c330cdc10b5b348e
曹桂凤 <demi.cao@oocl.com>
2019-08-17 20:15:44 +0800

    [Demi][feat:fix test7]

--- src/test/java/pokerHands/PokerHandsTest.java
+++ src/test/java/pokerHands/PokerHandsTest.java
@@ -104,5 +104,26 @@ public class PokerHandsTest {
 
     assertEquals("player 1 win!",result);
   }
+  @Test
+  public void should_return_player1_win_when_input_2H_3D_5S_9C_AD_and_2H_2D_5S_9C_KD() {
+
+    Card card1 = new Card("2H");
+    Card card2 = new Card("3D");
+    Card card3 = new Card("5S");
+    Card card4 = new Card("9C");
+    Card card5 = new Card("AD");
+    List<Card>cardList1 = Arrays.asList(card1,card2,card3,card4,card5);
+    Card card6 = new Card("2H");
+    Card card7 = new Card("2D");
+    Card card8 = new Card("5S");
+    Card card9 = new Card("9C");
+    Card card10 = new Card("KD");
+    List<Card>cardList2 = Arrays.asList(card6,card7,card8,card9,card10);
+
+    PokerHands pokerHands = new PokerHands();
+    String result = pokerHands.judge(cardList1,cardList2);
+
+    assertEquals("player 2 win!",result);
+  }
 
 }
