0ab949271499b6725619e85d413e3607a323a49c
曹桂凤 <demi.cao@oocl.com>
2019-08-17 16:52:09 +0800

    [Demi][feat:impelemnt test3 should_return_player1_win_when_input_KH_and_JH]

--- src/test/java/pokerHands/PokerHandsTest.java
+++ src/test/java/pokerHands/PokerHandsTest.java
@@ -36,4 +36,17 @@ public class PokerHandsTest {
     assertEquals("player 2 win!", result);
   }
 
+  @Test
+  public void should_return_player1_win_when_input_KH_and_JH() {
+
+    Card card1 = new Card("KH");
+    List<Card> cardList1 = Arrays.asList(card1);
+    Card card2 = new Card("JH");
+    List<Card> cardList2 = Arrays.asList(card2);
+
+    PokerHands pokerHands = new PokerHands();
+    String result = pokerHands.judge(cardList1, cardList2);
+
+    assertEquals("player 1 win!", result);
+  }
 }
